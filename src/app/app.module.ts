38c6628772f2dd887ac42a673b06aa753ae5d483
william.akel <william.akel@heart.org>
2021-10-22 09:07:22 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+
+
+  heros = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+}
